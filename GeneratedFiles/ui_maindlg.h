4a477512fbd1f500d7d6c11beeedb72a4b8a67bd
sssooonnnggg <sssooonnnggg111@gmail.com>
2015-03-29 17:55:18 +0800

    Change basic info object names

--- GeneratedFiles/ui_maindlg.h
+++ GeneratedFiles/ui_maindlg.h
@@ -77,37 +77,37 @@ public:
     QSpacerItem *horizontalSpacer_7;
     QVBoxLayout *verticalLayout_7;
     QHBoxLayout *horizontalLayout_3;
-    QLabel *label_11;
-    QLabel *label_12;
+    QLabel *label_001;
+    QLabel *label_001_content;
     QHBoxLayout *horizontalLayout_4;
-    QLabel *label_13;
-    QLabel *label_14;
+    QLabel *label_002;
+    QLabel *label_002_content;
     QHBoxLayout *horizontalLayout_5;
-    QLabel *label_15;
-    QLabel *label_16;
+    QLabel *label_003;
+    QLabel *label_003_content;
     QHBoxLayout *horizontalLayout_6;
-    QLabel *label_17;
-    QLabel *label_18;
+    QLabel *label_004;
+    QLabel *label_004_content;
     QHBoxLayout *horizontalLayout_7;
-    QLabel *label_19;
-    QLabel *label_20;
+    QLabel *label_005;
+    QLabel *label_005_content;
     QSpacerItem *horizontalSpacer_8;
     QVBoxLayout *verticalLayout_8;
     QHBoxLayout *horizontalLayout_8;
-    QLabel *label_21;
-    QLabel *label_22;
+    QLabel *label_006;
+    QLabel *label_006_content;
     QHBoxLayout *horizontalLayout_9;
-    QLabel *label_23;
-    QLabel *label_24;
+    QLabel *label_007;
+    QLabel *label_007_content;
     QHBoxLayout *horizontalLayout_10;
-    QLabel *label_25;
-    QLabel *label_26;
+    QLabel *label_008;
+    QLabel *label_008_content;
     QHBoxLayout *horizontalLayout_11;
-    QLabel *label_27;
-    QLabel *label_28;
+    QLabel *label_009;
+    QLabel *label_009_content;
     QHBoxLayout *horizontalLayout_12;
-    QLabel *label_29;
-    QLabel *label_30;
+    QLabel *label_010;
+    QLabel *label_010_content;
     QSpacerItem *horizontalSpacer_9;
     QLabel *label_7;
     QTextEdit *edtLog;
@@ -544,15 +544,15 @@ public:
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setSpacing(6);
         horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
-        label_11 = new QLabel(basic_info);
-        label_11->setObjectName(QString::fromUtf8("label_11"));
+        label_001 = new QLabel(basic_info);
+        label_001->setObjectName(QString::fromUtf8("label_001"));
 
-        horizontalLayout_3->addWidget(label_11);
+        horizontalLayout_3->addWidget(label_001);
 
-        label_12 = new QLabel(basic_info);
-        label_12->setObjectName(QString::fromUtf8("label_12"));
+        label_001_content = new QLabel(basic_info);
+        label_001_content->setObjectName(QString::fromUtf8("label_001_content"));
 
-        horizontalLayout_3->addWidget(label_12);
+        horizontalLayout_3->addWidget(label_001_content);
 
 
         verticalLayout_7->addLayout(horizontalLayout_3);
@@ -560,15 +560,15 @@ public:
         horizontalLayout_4 = new QHBoxLayout();
         horizontalLayout_4->setSpacing(6);
         horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
-        label_13 = new QLabel(basic_info);
-        label_13->setObjectName(QString::fromUtf8("label_13"));
+        label_002 = new QLabel(basic_info);
+        label_002->setObjectName(QString::fromUtf8("label_002"));
 
-        horizontalLayout_4->addWidget(label_13);
+        horizontalLayout_4->addWidget(label_002);
 
-        label_14 = new QLabel(basic_info);
-        label_14->setObjectName(QString::fromUtf8("label_14"));
+        label_002_content = new QLabel(basic_info);
+        label_002_content->setObjectName(QString::fromUtf8("label_002_content"));
 
-        horizontalLayout_4->addWidget(label_14);
+        horizontalLayout_4->addWidget(label_002_content);
 
 
         verticalLayout_7->addLayout(horizontalLayout_4);
@@ -576,15 +576,15 @@ public:
         horizontalLayout_5 = new QHBoxLayout();
         horizontalLayout_5->setSpacing(6);
         horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
-        label_15 = new QLabel(basic_info);
-        label_15->setObjectName(QString::fromUtf8("label_15"));
+        label_003 = new QLabel(basic_info);
+        label_003->setObjectName(QString::fromUtf8("label_003"));
 
-        horizontalLayout_5->addWidget(label_15);
+        horizontalLayout_5->addWidget(label_003);
 
-        label_16 = new QLabel(basic_info);
-        label_16->setObjectName(QString::fromUtf8("label_16"));
+        label_003_content = new QLabel(basic_info);
+        label_003_content->setObjectName(QString::fromUtf8("label_003_content"));
 
-        horizontalLayout_5->addWidget(label_16);
+        horizontalLayout_5->addWidget(label_003_content);
 
 
         verticalLayout_7->addLayout(horizontalLayout_5);
@@ -592,15 +592,15 @@ public:
         horizontalLayout_6 = new QHBoxLayout();
         horizontalLayout_6->setSpacing(6);
         horizontalLayout_6->setObjectName(QString::fromUtf8("horizontalLayout_6"));
-        label_17 = new QLabel(basic_info);
-        label_17->setObjectName(QString::fromUtf8("label_17"));
+        label_004 = new QLabel(basic_info);
+        label_004->setObjectName(QString::fromUtf8("label_004"));
 
-        horizontalLayout_6->addWidget(label_17);
+        horizontalLayout_6->addWidget(label_004);
 
-        label_18 = new QLabel(basic_info);
-        label_18->setObjectName(QString::fromUtf8("label_18"));
+        label_004_content = new QLabel(basic_info);
+        label_004_content->setObjectName(QString::fromUtf8("label_004_content"));
 
-        horizontalLayout_6->addWidget(label_18);
+        horizontalLayout_6->addWidget(label_004_content);
 
 
         verticalLayout_7->addLayout(horizontalLayout_6);
@@ -608,15 +608,15 @@ public:
         horizontalLayout_7 = new QHBoxLayout();
         horizontalLayout_7->setSpacing(6);
         horizontalLayout_7->setObjectName(QString::fromUtf8("horizontalLayout_7"));
-        label_19 = new QLabel(basic_info);
-        label_19->setObjectName(QString::fromUtf8("label_19"));
+        label_005 = new QLabel(basic_info);
+        label_005->setObjectName(QString::fromUtf8("label_005"));
 
-        horizontalLayout_7->addWidget(label_19);
+        horizontalLayout_7->addWidget(label_005);
 
-        label_20 = new QLabel(basic_info);
-        label_20->setObjectName(QString::fromUtf8("label_20"));
+        label_005_content = new QLabel(basic_info);
+        label_005_content->setObjectName(QString::fromUtf8("label_005_content"));
 
-        horizontalLayout_7->addWidget(label_20);
+        horizontalLayout_7->addWidget(label_005_content);
 
 
         verticalLayout_7->addLayout(horizontalLayout_7);
@@ -634,15 +634,15 @@ public:
         horizontalLayout_8 = new QHBoxLayout();
         horizontalLayout_8->setSpacing(6);
         horizontalLayout_8->setObjectName(QString::fromUtf8("horizontalLayout_8"));
-        label_21 = new QLabel(basic_info);
-        label_21->setObjectName(QString::fromUtf8("label_21"));
+        label_006 = new QLabel(basic_info);
+        label_006->setObjectName(QString::fromUtf8("label_006"));
 
-        horizontalLayout_8->addWidget(label_21);
+        horizontalLayout_8->addWidget(label_006);
 
-        label_22 = new QLabel(basic_info);
-        label_22->setObjectName(QString::fromUtf8("label_22"));
+        label_006_content = new QLabel(basic_info);
+        label_006_content->setObjectName(QString::fromUtf8("label_006_content"));
 
-        horizontalLayout_8->addWidget(label_22);
+        horizontalLayout_8->addWidget(label_006_content);
 
 
         verticalLayout_8->addLayout(horizontalLayout_8);
@@ -650,15 +650,15 @@ public:
         horizontalLayout_9 = new QHBoxLayout();
         horizontalLayout_9->setSpacing(6);
         horizontalLayout_9->setObjectName(QString::fromUtf8("horizontalLayout_9"));
-        label_23 = new QLabel(basic_info);
-        label_23->setObjectName(QString::fromUtf8("label_23"));
+        label_007 = new QLabel(basic_info);
+        label_007->setObjectName(QString::fromUtf8("label_007"));
 
-        horizontalLayout_9->addWidget(label_23);
+        horizontalLayout_9->addWidget(label_007);
 
-        label_24 = new QLabel(basic_info);
-        label_24->setObjectName(QString::fromUtf8("label_24"));
+        label_007_content = new QLabel(basic_info);
+        label_007_content->setObjectName(QString::fromUtf8("label_007_content"));
 
-        horizontalLayout_9->addWidget(label_24);
+        horizontalLayout_9->addWidget(label_007_content);
 
 
         verticalLayout_8->addLayout(horizontalLayout_9);
@@ -666,15 +666,15 @@ public:
         horizontalLayout_10 = new QHBoxLayout();
         horizontalLayout_10->setSpacing(6);
         horizontalLayout_10->setObjectName(QString::fromUtf8("horizontalLayout_10"));
-        label_25 = new QLabel(basic_info);
-        label_25->setObjectName(QString::fromUtf8("label_25"));
+        label_008 = new QLabel(basic_info);
+        label_008->setObjectName(QString::fromUtf8("label_008"));
 
-        horizontalLayout_10->addWidget(label_25);
+        horizontalLayout_10->addWidget(label_008);
 
-        label_26 = new QLabel(basic_info);
-        label_26->setObjectName(QString::fromUtf8("label_26"));
+        label_008_content = new QLabel(basic_info);
+        label_008_content->setObjectName(QString::fromUtf8("label_008_content"));
 
-        horizontalLayout_10->addWidget(label_26);
+        horizontalLayout_10->addWidget(label_008_content);
 
 
         verticalLayout_8->addLayout(horizontalLayout_10);
@@ -682,15 +682,15 @@ public:
         horizontalLayout_11 = new QHBoxLayout();
         horizontalLayout_11->setSpacing(6);
         horizontalLayout_11->setObjectName(QString::fromUtf8("horizontalLayout_11"));
-        label_27 = new QLabel(basic_info);
-        label_27->setObjectName(QString::fromUtf8("label_27"));
+        label_009 = new QLabel(basic_info);
+        label_009->setObjectName(QString::fromUtf8("label_009"));
 
-        horizontalLayout_11->addWidget(label_27);
+        horizontalLayout_11->addWidget(label_009);
 
-        label_28 = new QLabel(basic_info);
-        label_28->setObjectName(QString::fromUtf8("label_28"));
+        label_009_content = new QLabel(basic_info);
+        label_009_content->setObjectName(QString::fromUtf8("label_009_content"));
 
-        horizontalLayout_11->addWidget(label_28);
+        horizontalLayout_11->addWidget(label_009_content);
 
 
         verticalLayout_8->addLayout(horizontalLayout_11);
@@ -698,15 +698,15 @@ public:
         horizontalLayout_12 = new QHBoxLayout();
         horizontalLayout_12->setSpacing(6);
         horizontalLayout_12->setObjectName(QString::fromUtf8("horizontalLayout_12"));
-        label_29 = new QLabel(basic_info);
-        label_29->setObjectName(QString::fromUtf8("label_29"));
+        label_010 = new QLabel(basic_info);
+        label_010->setObjectName(QString::fromUtf8("label_010"));
 
-        horizontalLayout_12->addWidget(label_29);
+        horizontalLayout_12->addWidget(label_010);
 
-        label_30 = new QLabel(basic_info);
-        label_30->setObjectName(QString::fromUtf8("label_30"));
+        label_010_content = new QLabel(basic_info);
+        label_010_content->setObjectName(QString::fromUtf8("label_010_content"));
 
-        horizontalLayout_12->addWidget(label_30);
+        horizontalLayout_12->addWidget(label_010_content);
 
 
         verticalLayout_8->addLayout(horizontalLayout_12);
@@ -732,7 +732,7 @@ public:
 "	min-height:30px;\n"
 "	padding-left:10px;\n"
 "	border:1px solid #cccccc;\n"
-"	border-bottom-width:0px;\n"
+"	border-bottom-width:1px;\n"
 "	border-left-width:0px;\n"
 "}"));
 
@@ -741,6 +741,7 @@ public:
         edtLog = new QTextEdit(logFrame);
         edtLog->setObjectName(QString::fromUtf8("edtLog"));
         edtLog->setMaximumSize(QSize(16777215, 300));
+        edtLog->setFrameShape(QFrame::NoFrame);
         edtLog->setReadOnly(true);
 
         verticalLayout->addWidget(edtLog);
@@ -1054,9 +1055,6 @@ public:
         verticalLayout_6->addWidget(pluginStatus);
 
         MainDlgClass->setCentralWidget(centralWidget);
-        label_4->raise();
-        tbBasicInfo->raise();
-        edtLog->raise();
         mainToolBar = new QToolBar(MainDlgClass);
         mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
         mainToolBar->setMovable(false);
@@ -1218,26 +1216,26 @@ public:
         ___qtablewidgetitem6->setText(QApplication::translate("MainDlgClass", "\346\226\260\345\273\272\350\241\214", 0, QApplication::UnicodeUTF8));
         label_9->setText(QApplication::translate("MainDlgClass", "\350\257\246\347\273\206\344\277\241\346\201\257", 0, QApplication::UnicodeUTF8));
         label_10->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_11->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_12->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_13->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_14->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_15->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_16->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_17->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_18->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_19->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_20->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_21->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_22->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_23->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_24->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_25->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_26->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_27->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_28->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_29->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
-        label_30->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_001->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_001_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_002->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_002_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_003->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_003_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_004->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_004_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_005->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_005_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_006->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_006_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_007->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_007_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_008->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_008_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_009->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_009_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_010->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
+        label_010_content->setText(QApplication::translate("MainDlgClass", "TextLabel", 0, QApplication::UnicodeUTF8));
         label_7->setText(QApplication::translate("MainDlgClass", "\350\277\220\350\241\214\350\256\260\345\275\225", 0, QApplication::UnicodeUTF8));
         label_3->setText(QApplication::translate("MainDlgClass", "\347\233\256\346\240\207\346\200\273\346\225\260 : ", 0, QApplication::UnicodeUTF8));
         lbTargetCount->setText(QApplication::translate("MainDlgClass", "0", 0, QApplication::UnicodeUTF8));
